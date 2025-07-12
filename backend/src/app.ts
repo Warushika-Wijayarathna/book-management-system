29d45871b9eb6a4582f880bd953de0d4c161e1d0
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-12 22:08:14 +0530

    basic set up done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,39 @@
 import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import rootRouter from "./routes";
+import {errorHandler} from "./middlewares/errorHandler";
+import {connectDB} from "./db/mongo";
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+dotenv.config()
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// unable cors configuration
+const corsOperations = {
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+app.use(cors(corsOperations));
+
+app.use(express.json());
+// handling JSON requests
+app.use(cookieParser());
+
+// calling api endpoints
+app.use("/api", rootRouter)
+// error handling middleware
+app.use(errorHandler)
+
+// connects MongoDB to the app
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from "express";
+import {APIError} from "../errors/APIError";
+import mongoose from "mongoose";
+
+export const errorHandler= (error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof mongoose.Error) {
+        res.status(400).json({message: error.message});
+        return
+    }
+
+    if (error instanceof APIError) {
+        res.status(error.status).json({message: error.message});
+        return
+    }
+
+    res.status(500).json({message: "Internal Server Error"});
+}
