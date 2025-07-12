faa7e784d414fac359bb3a80e7262ec68572450f
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-12 19:40:56 +0530

    backend structured with middleware setup

---
+++ backend/src/middlewares/authenticateToken.ts
@@ -0,0 +1,35 @@
+import jwt, {JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
+import {NextFunction, Request, Response} from "express";
+import {APIError} from "../errors/APIError";
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authHeader =  req.headers["authorization"]
+        const token =  authHeader && authHeader.split(" ")[1]
+
+        if (!token) {
+            throw new APIError(403, "Access token not found")
+        }
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (error, decoded) => {
+            if (error) {
+                if (error instanceof TokenExpiredError) {
+                    throw new APIError(403, "Access token expired")
+                } else if (error instanceof JsonWebTokenError) {
+                    throw new APIError(403, "Invalid access token")
+                } else {
+                    throw new APIError(403, "Could not authenticate token")
+                }
+            }
+
+            if (!decoded || typeof decoded === "string") {
+                throw new APIError(403, "Invalid token payload")
+            }
+
+            next();
+        })
+
+    } catch (error) {
+        next(error);
+    }
+}
