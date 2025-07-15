73305d9570fce4f88166b07641de694c43bc85d1
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-15 21:56:42 +0530

    models are created - modified

---
+++ backend/src/models/AuditLog.ts
@@ -0,0 +1,35 @@
+import mongoose, { Types } from "mongoose";
+
+type AuditLog = {
+    action: string;
+    performedBy: Types.ObjectId;
+    targetModel: string;
+    targetId: Types.ObjectId;
+    timestamp: Date;
+}
+
+const auditLogSchema = new mongoose.Schema({
+    action: {
+        type: String,
+        required: [true, "Action is required"],
+        trim: true
+    },
+    performedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: [true, "Performed by is required"]
+    },
+    targetModel: {
+        type: String,
+        required: [true, "Target model is required"],
+        trim: true
+    },
+    targetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "Target ID is required"]
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
