aff753ccf09c1f7b00699f1bb09a2c09f0b6672f
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-16 18:28:25 +0530

    overdue.routes added

---
+++ backend/src/routes/overdue.routes.ts
@@ -0,0 +1,9 @@
+import {Router} from "express"
+import {getOverdueByReader, getOverdueReaders} from "../controllers/overdue.controller";
+
+export const overdueRouter = Router()
+
+overdueRouter.get("/", getOverdueReaders)
+overdueRouter.get("/:id", getOverdueByReader)
+
+export default overdueRouter
