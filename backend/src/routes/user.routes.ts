a77fd9aa7d6d246a9b2d16e8f637389a815c56fb
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-16 08:35:59 +0530

    user.routes added

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import {authenticateToken} from "../middlewares/authenticateToken";
+import {getUser, updateUser} from "../controllers/user.controller";
+
+export const userRouter = Router()
+
+userRouter.use(authenticateToken)
+
+userRouter.get("/", getUser)
+userRouter.put("/:id", updateUser)
+
+export default userRouter;
