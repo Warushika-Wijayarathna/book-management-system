2260207fef1d3ea4501bd97d51e64303b4adad79
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-07-16 22:04:45 +0530

    all routers added to the rootRouter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,20 @@
 import {Router} from "express"
+import authRouter from "./auth.routes"
+import bookRouter from "./book.routes"
+import readerRouter from "./reader.routes"
+import lendingRouter from "./lending.routes"
+import notificationRouter from "./notification.routes"
+import overdueRouter from "./overdue.routes"
+import userRouter from "./user.routes"
 
 const rootRouter = Router()
+
+rootRouter.use("/auth", authRouter)
+rootRouter.use("/books", bookRouter)
+rootRouter.use("/lendings", lendingRouter)
+rootRouter.use("/notifications", notificationRouter)
+rootRouter.use("/overdue", overdueRouter)
+rootRouter.use("/readers", readerRouter)
+rootRouter.use("/users", userRouter)
 
 export default rootRouter
